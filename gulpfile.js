5d65b42e2cdf941fd510db2a5e7670fefce9f3ae
neptunus <neptunus@hirt.is>
2017-07-04 14:43:03 +0000

    changed locales

--- gulpfile.js
+++ gulpfile.js
@@ -13,67 +13,26 @@ const path = require('path');
 const Cldr = require('cldrjs');
 
 const locales = [
-  'am',           // Amharic
-  'ar',           // Arabic
-  'bg',           // Bulgarian
-  'bn',           // Bengali
-  'ca',           // Catalan
-  'cs',           // Czech
   'da',           // Danish
   'de',           // German
-  'el',           // Greek
   'en',           // English (United States)
   'en-GB',        // English (Great Britain)
-  'es',           // Spanish
-  'es-419',       // Spanish (Latin America & Caribbean)
-  'et',           // Estonian
-  'fa',           // Persian
-  'fi',           // Finnish
-  'fil',          // Filipino
   'fr',           // French
-  'gu',           // Gujarati
-  'he',           // Hebrew
-  'hi',           // Hindi
-  'hr',           // Croatian
-  'hu',           // Hungarian
-  'id',           // Indonesian
+  'is',           // Icelandic
   'it',           // Italian
-  'ja',           // Japanese
-  'kn',           // Kannada
-  'ko',           // Korean
-  'lt',           // Lithuanian
-  'lv',           // Latvian
-  'ml',           // Malayalam
-  'mr',           // Marathi
-  'ms',           // Malay
   'nb',           // Norwegian
   'nl',           // Dutch
-  'pl',           // Polish
-  'pt',           // Portuguese
-  'pt-PT',        // Portuguese (Portugal)
-  'ro',           // Romanian
-  'ru',           // Russian
-  'sk',           // Slovak
-  'sl',           // Slovenian
-  'sr',           // Serbian
   'sv',           // Swedish
-  'sw',           // Swahili
-  'ta',           // Tamil
-  'te',           // Telugu
-  'th',           // Thai
-  'tr',           // Turkish
-  'uk',           // Ukrainian
-  'vi',           // Vietnamese
-  'zh',           // Chinese
-  'zh-Hans',      // Chinese (Simplified)
-  'zh-Hant',      // Chinese (Traditional)
 ];
 
 const currencies = [
-  'CAD',          // Canadian Dollar
   'EUR',          // Euro
   'GBP',          // British Pound
   'USD',          // US Dollar
+  'ISK',          // Icelandic Krona
+  'DKK',          // Danish Krona
+  'NOK',          // Norwegian Krona
+  'SEK',          // Swedish Krona
 ];
 
 const files = ['ca-gregorian', 'currencies', 'dateFields', 'numbers', 'timeZoneNames'];
